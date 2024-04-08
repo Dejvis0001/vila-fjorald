ac664e273117bfa056d11315f920439796b07a53
Dejvis0001 <devimyrto26@outlook.com>
2024-04-08 21:01:37 +0200

    remmake , rooms not finished

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-//NavBar//
-const navEl = document.querySelector(".navbar");
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= 56) {
-    navEl.classList.add("navbar-scrolled");
-  } else {
-    navEl.classList.remove("navbar-scrolled");
-  }
-});
 //Carousel//
 $(".carousel").carousel({
   interval: 2000,
